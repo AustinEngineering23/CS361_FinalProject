412de07e02c3955d84ef838879207c885ffc7102
Kyle Harris <harr1193@msu.edu>
2019-12-05 01:30:13 -0500

    login/signup auth and send to db

--- app.js
+++ app.js
@@ -1,41 +1,95 @@
-var express = require('express');
-var mysql = require('./dbcon.js');
-var app = express();
+const express = require('express');
+const app = express();
+const mysql = require('./dbcon.js');
 const path = require('path');
-const router = express.Router();
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
-app.set('port', process.env.PORT || 8080);
+
+app.set('port', process.env.PORT || 9025);
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
 
 //to render the login page homepage
 app.get('/', function(req,res,next){
 	res.sendFile(__dirname + '/public/login.html');
 });
 
-app.get('/login', function(req,res,next) {
-	// var username = req.body.username;
-	// var password =  req.body.password;
-	// var context = {};
-	// var sqlString = "SELECT * from `Users` WHERE username = ? AND password = ?", username, password;
-	res.redirect('/login_success');
+app.post('/auth', function(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	if (username && password) {
+		mysql.pool.query('SELECT * FROM Users WHERE username = ? AND password = ?', [username, password], function(err, results, fields) {
+			if (results.length > 0) {
+				req.session.loggedin = true;
+				req.session.username = username;
+				res.redirect('/login_success')
+			} else {
+				res.redirect('/signup-page');
+			}
+			res.end();
+		});
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+});
+
+app.post('/signup', function(req,res,next) {	
+	var username = req.body.username;
+	var password = req.body.password;
+	var fName = req.body.fname;
+	var lName = req.body.lname;
+	var email = req.body.email;
+	var phone = req.body.phone;
+	var address = req.body.address;
+
+	if (username && password) {
+		mysql.pool.query('INSERT INTO `Users` (`username`, `password`, `fname`, `lname`, `email`, `phone`, `address`) VALUES(?,?,?,?,?,?,?)', 
+			[username, password, fName, lName, email, phone, address], function(err, results, fields) {
+			req.session.loggedin = true;
+			req.session.username = username;
+			res.redirect('/signup_success');
+		});
+	} else {
+		res.send('Email or Password missing');
+	}
+	
+});
+
+app.get('/report_page', function(req,res,next) {
+	res.sendFile(__dirname + '/public/report_page.html');
 });
 
 app.get('/login_success', function(req,res,next) {
-	res.sendFile(__dirname + '/public/homepage.html');
+	if (req.session.loggedin) {
+		res.sendFile(__dirname + '/public/homepage.html');
+	} else {
+		res.send('Please login to view this page');
+	}
 });
 
 app.get('/signup-page', function(req,res,next) {
 	res.sendFile(__dirname + '/public/signup.html');
 });
 
-app.get('/signup', function(req,res,next) {
-	res.redirect('/signup_success');
-});
-
 app.get('/signup_success', function(req,res,next) {
-	res.sendFile(__dirname + '/public/homepage.html');
+	if (req.session.loggedin) {
+		res.sendFile(__dirname + '/public/homepage.html');
+	} else {
+		res.send('Please login to view this page');
+	}
 });
 app.get('/logout', function(req,res,next) {
+	req.session.loggedin = false;
+	req.session.username = null;
 	res.redirect('/');
 });
 app.get('/adminLogin', function(req,res,next) {
@@ -44,19 +98,6 @@ app.get('/adminLogin', function(req,res,next) {
 app.get('/deviceView', function(req,res,next){
 	res.sendFile(__dirname + '/public/deviceView.html');
 });
-
-// app.use(function(req,res){
-//   res.status(404);
-//   res.render('404');
-// });
-
-// app.use(function(err, req, res, next){
-//   console.error(err.stack);
-//   res.status(500);
-//   res.render('500');
-// });
-
-app.use('/', router);
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit 	: 10,
-  host            	: 'classmysql.engr.oregonstate.edu',
-  user 				: 'cs361_',
+  host            	: '',
+  user 				: '',
   password			: '',
-  database			: 'cs361' 
+  database			: '' 
 });
 
 module.exports.pool = pool;
